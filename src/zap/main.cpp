8c9f203a5a79f0fa381576ffe52f309544249521
skyitachi <skbysp@gmail.com>
2022-04-23 16:02:11 +0800

    add clang-tidy and clang-format

--- src/zap/main.cpp
+++ src/zap/main.cpp
@@ -1,12 +1,13 @@
-#include <iostream>
 #include "segment.h"
+#include <iostream>
 
 int main() {
   Segment seg_{};
-  bool is_success = seg_.Open("/Users/skyitachi/lab/index-explorer/cmake_build/src/000000000002.zap");
+  bool is_success = seg_.Open(
+      "/Users/skyitachi/lab/index-explorer/cmake_build/src/000000000002.zap");
 
   if (is_success) {
-    const Footer& footer = seg_.parseFooter();
+    const Footer &footer = seg_.parseFooter();
     std::cout << footer.describe() << std::endl;
     return 0;
   }
--- src/zap/segment.h
+++ src/zap/segment.h
@@ -4,38 +4,38 @@
 #include <fmt/format.h>
 
 struct Footer {
-	uint64_t D;      // Number of Docs
-	uint64_t SF;		// stored index offset
-	uint64_t F;      // field index offset
-	uint64_t DOC;    // doc value offset 
-	uint32_t CF;		// chunk offset
-	uint32_t Ver;   // version
-	uint32_t CC;     // crc32
+  uint64_t D;   // Number of Docs
+  uint64_t SF;  // stored index offset
+  uint64_t F;   // field index offset
+  uint64_t DOC; // doc value offset
+  uint32_t CF;  // chunk offset
+  uint32_t Ver; // version
+  uint32_t CC;  // crc32
 
-public :
-	const std::string describe() const {
-		return fmt::format("number_of_docs={}, stored_index_offset={}, "
-						   "field_index_offset={}, doc_value_offset={}, "
-		                   "chunk_offset={}, version={}, crc={}", 
-						   D, SF, F, DOC, CF, Ver, CC);	
-	}
+public:
+  const std::string describe() const {
+    return fmt::format("number_of_docs={}, stored_index_offset={}, "
+                       "field_index_offset={}, doc_value_offset={}, "
+                       "chunk_offset={}, version={}, crc={}",
+                       D, SF, F, DOC, CF, Ver, CC);
+  }
 };
 
 class Segment {
 
 public:
-	bool Open(const std::string& path);
-	const Footer& parseFooter();	
+  bool Open(const std::string &path);
+  const Footer &parseFooter();
 
-	~Segment() {
-		if (is_opened_) {
-			munmap(data_, sz_);
-		}
-	}
+  ~Segment() {
+    if (is_opened_) {
+      munmap(data_, sz_);
+    }
+  }
 
 private:
-	bool is_opened_;
-	size_t sz_;
-	char* data_ = nullptr;
-	Footer footer_;
+  bool is_opened_;
+  size_t sz_;
+  char *data_ = nullptr;
+  Footer footer_;
 };
